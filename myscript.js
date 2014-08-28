8a285300add929350e483a63bf190b8ae4fca792
Damir Svrtan <damir.svrtan@gmail.com>
2014-08-29 01:36:49 +0200

    Click button in top right corner.

---
+++ myscript.js
@@ -0,0 +1,6 @@
+var btn = document.createElement("BUTTON")
+btn.className = 'wwz'
+var t = document.createTextNode("CLICK ME");
+btn.appendChild(t);
+//Appending to DOM
+document.body.appendChild(btn);
---
+++ mystyles.js
@@ -0,0 +1,6 @@
+.wwz{
+  position: absolute;
+  margin-top: 0px;
+  top: 0px;
+  right: 0px;
+}
