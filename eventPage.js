7426e6632728f3e2f59565f67a2ba25079cfd764
Damir Svrtan <damir.svrtan@gmail.com>
2014-08-29 20:33:10 +0200

    WIP: showing ribbon only when host stored in chrome storage.

--- eventPage.js
+++ eventPage.js
@@ -1,56 +1,34 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  var status = false;
-  sendResponse({status: hejurl()});
-});
+/* global chrome, document, alert, console */
+'use strict';
 
-var hejurl = function(){
-  vubi = chrome.storage.local.get({productionURLs: []}, function (result) {
-    productionURLs = result.productionURLs;
-
-    if(productionURLs.indexOf(request.url) != 1){
-      // return true;
-    }else{
-      // return false;
-    };
-    return 1;
-  });
-
-  return vubi;
-};
+console.log('eventpage');
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   chrome.storage.local.get({productionURLs: []}, function (result) {
+//     var productionURLs = result.productionURLs;
+//     var status = false;
+//     if(productionURLs.indexOf(request.url) != 1){
+//       status = true;
+//     }
+//   });
+// });
 
 chrome.browserAction.onClicked.addListener(function(tab){
   var parser = document.createElement('a');
   parser.href = tab.url;
   // alert(parser.hostname);
 
-// chrome.storage.local.get({markedURLs: []}, function (result) {
-//     // the input argument is ALWAYS an object containing the queried keys
-//     // so we select the key we need
-//     var markedURLs = result.markedURLs;
-//     markedURLs.push({keyPairId: keyPairId, HasBeenUploadedYet: false});
-//     // set the new array value to the same key
-//     chrome.storage.local.set({markedURLs: markedURLs}, function () {
-//         // you can use strings instead of objects
-//         // if you don't  want to define default values
-//         chrome.storage.local.get('markedURLs', function (result) {
-//             console.log(result.markedURLs)
-//         });
-//     });
-// });
-
-
-
   chrome.storage.local.get({productionURLs: []}, function (result) {
 
     var productionURLs = result.productionURLs;
 
     var index = productionURLs.indexOf(parser.hostname);
+    var icon_path;
 
     if(index == -1){
       productionURLs.push(parser.hostname);
       icon_path = 'danger-triangle-red-128.png';
     }else{
-      icon_path = 'danger-triangle-128.png'
+      icon_path = 'danger-triangle-128.png';
       productionURLs.splice(index, 1);
     }
 
@@ -60,12 +38,9 @@ chrome.browserAction.onClicked.addListener(function(tab){
     });
 
     chrome.storage.local.set({productionURLs: productionURLs}, function(){
-      chrome.storage.local.get({productionURLs: []}, function (result) {
-        alert(JSON.stringify(result.productionURLs));
-      });
+      console.log(JSON.stringify(productionURLs));
     });
 
   });
-  // alert('ee');
   }
 );
--- myscript.js
+++ myscript.js
@@ -1,23 +1,35 @@
-chrome.runtime.sendMessage({method: "getStatus", url: location.host}, function(response) {
-  console.log(response.status);
-});
+var showRibbon = function(){
+  var ribbonWrapper = document.createElement("div");
+  ribbonWrapper.className = 'github-fork-ribbon-wrapper right fixed';
+  ribbonWrapper.onclick = function(){
+    this.style.display='none';
+  };
 
-var ribbonWrapper = document.createElement("div");
-ribbonWrapper.className = 'github-fork-ribbon-wrapper right fixed';
-ribbonWrapper.onclick = function(){
-  this.style.display='none';
+  var ribbon = document.createElement("div");
+  ribbon.className = 'github-fork-ribbon red';
+
+  var ribbonText = document.createElement("span");
+  ribbonText.className = 'github-fork-ribbon-text';
+
+  var text = document.createTextNode("Production");
+
+  ribbonText.appendChild(text);
+  ribbon.appendChild(ribbonText);
+  ribbonWrapper.appendChild(ribbon);
+
+  document.body.appendChild(ribbonWrapper);
 };
 
-var ribbon = document.createElement("div");
-ribbon.className = 'github-fork-ribbon red';
 
-var ribbonText = document.createElement("span");
-ribbonText.className = 'github-fork-ribbon-text'
+chrome.storage.local.get({productionURLs: []}, function (result) {
+    var productionURLs = result.productionURLs;
+    var status = false;
+    if(productionURLs.indexOf(location.host) != -1){
+      showRibbon();
+    }
+    console.log(JSON.stringify(productionURLs));
+});
 
-var text = document.createTextNode("Production");
-
-ribbonText.appendChild(text);
-ribbon.appendChild(ribbonText);
-ribbonWrapper.appendChild(ribbon);
-
-document.body.appendChild(ribbonWrapper);
+// chrome.runtime.sendMessage({method: "getStatus", url: location.host}, function(response) {
+//   console.log(arguments);
+// });
